d04e002634942a48db4bc5bfee05e3f820ce8494
MHMIZAN09 <mhmizan.cse.gub@gmail.com>
2023-10-03 09:15:01 +0600

    React dragon news paper

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHvrbAgmDGmcbXxT-0RQu8LJJzbEo7J5s",
+  authDomain: "dragon-news-react-48f77.firebaseapp.com",
+  projectId: "dragon-news-react-48f77",
+  storageBucket: "dragon-news-react-48f77.appspot.com",
+  messagingSenderId: "746209210737",
+  appId: "1:746209210737:web:dcdaca28c568c123b186d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
